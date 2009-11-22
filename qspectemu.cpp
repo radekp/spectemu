aac422b43e9ceb609b7edd081ddb50396d1c28b2
Radek Polak <psonek2@seznam.cz>
2009-11-22 15:15:00 +0100

    better keyboard and screen updating

--- qspectemu.cpp
+++ qspectemu.cpp
@@ -2,7 +2,8 @@
 #include "spkey_p.h"
 
 QSpectemu *qspectemu;
-QImage scr(TV_WIDTH, TV_HEIGHT, QImage::Format_ARGB32);
+QImage scr;
+bool updating;
 
 QSpectemu::QSpectemu(QWidget *parent, Qt::WFlags f)
     : QWidget(parent)
@@ -16,6 +17,7 @@ QSpectemu::QSpectemu(QWidget *parent, Qt::WFlags f)
 
     argc = 0;
     argv = 0;
+    updating = false;
     qspectemu = this;
     QTimer::singleShot(1, this, SLOT(startSpectemu()));
 }
@@ -62,6 +64,7 @@ void QSpectemu::paintEvent(QPaintEvent *e)
     }
     QPainter p(this);
     p.drawImage(0, 0, scr);
+    updating = false;
 }
 
 void QSpectemu::mousePressEvent(QMouseEvent *e)
@@ -69,17 +72,80 @@ void QSpectemu::mousePressEvent(QMouseEvent *e)
 
 }
 
+bool decodeKey(int key, int *ks, int *shks, int *ki)
+{
+    *shks = 0;
+    switch(key)
+    {
+        case Qt::Key_Escape:    *ks = SK_Escape;                        break;
+        case Qt::Key_1:         *ks = '1';          *shks = '!';        break;
+        case Qt::Key_2:         *ks = '2';          *shks = '@';        break;
+        case Qt::Key_3:         *ks = '3';          *shks = '#';        break;
+        case Qt::Key_4:         *ks = '4';          *shks = '$';        break;
+        case Qt::Key_5:         *ks = '5';          *shks = '%';        break;
+        case Qt::Key_6:         *ks = '6';          *shks = '^';        break;
+        case Qt::Key_7:         *ks = '7';          *shks = '&';        break;
+        case Qt::Key_8:         *ks = '8';          *shks = '*';        break;
+        case Qt::Key_9:         *ks = '9';          *shks = '(';        break;
+        case Qt::Key_0:         *ks = '0';          *shks = ')';        break;
+        case Qt::Key_Minus:     *ks = '-';          *shks = '_';        break;
+        case Qt::Key_Equal:     *ks = '=';          *shks = '+';        break;
+        case Qt::Key_Backspace: *ks = SK_BackSpace;                     break;
+        case Qt::Key_Tab:       *ks = SK_Tab;                           break;
+        case Qt::Key_Q:         *ks = 'q';          *shks = 'Q';        break;
+        case Qt::Key_W:         *ks = 'w';          *shks = 'W';        break;
+        case Qt::Key_E:         *ks = 'e';          *shks = 'E';        break;
+        case Qt::Key_R:         *ks = 'r';          *shks = 'R';        break;
+        case Qt::Key_T:         *ks = 't';          *shks = 'T';        break;
+        case Qt::Key_Y:         *ks = 'y';          *shks = 'Y';        break;
+        case Qt::Key_U:         *ks = 'u';          *shks = 'U';        break;
+        case Qt::Key_I:         *ks = 'i';          *shks = 'I';        break;
+        case Qt::Key_O:         *ks = 'o';          *shks = 'O';        break;
+        case Qt::Key_P:         *ks = 'p';          *shks = 'P';        break;
+        case Qt::Key_BracketLeft:   *ks = '[';      *shks = '{';        break;
+        case Qt::Key_BracketRight:  *ks = ']';      *shks = '}';        break;
+        case Qt::Key_Return:    *ks = SK_Return;                        break;
+        case Qt::Key_Control:   *ks = SK_Control_L;                        break;
+        case Qt::Key_A:  *ks = 'a';      *shks = 'A';        break;
+        case Qt::Key_S:  *ks = 's';      *shks = 'S';        break;
+        case Qt::Key_D:  *ks = 'd';      *shks = 'D';        break;
+        case Qt::Key_F:  *ks = 'f';      *shks = 'F';        break;
+        case Qt::Key_G:  *ks = 'g';      *shks = 'G';        break;
+        case Qt::Key_H:  *ks = 'h';      *shks = 'H';        break;
+        case Qt::Key_J:  *ks = 'j';      *shks = 'J';        break;
+        case Qt::Key_K:  *ks = 'k';      *shks = 'K';        break;
+        case Qt::Key_L:  *ks = 'l';      *shks = 'L';        break;
+        case Qt::Key_Semicolon:  *ks = ';';      *shks = ':';        break;
+        case Qt::Key_Apostrophe: *ks = '\'';     *shks = '"';        break;
+        case Qt::Key_QuoteLeft:  *ks = '`';      *shks = '~';        break;
+        default:
+            return false;
+    }
+    if(*shks == 0)
+    {
+        *shks = *ks;
+    }
+    *ki = KS_TO_KEY(*ks);
+    return true;
+}
+
 void QSpectemu::keyPressEvent(QKeyEvent *e)
 {
-    int key = e->key();
-    spkb_last.keysym = key;
-    spkb_last.shifted = key;
-    spkb_last.modif = 0;
-    spkb_last.index = key;
+    int ks, shks, ki;
+    if(!decodeKey(e->key(), &ks, &shks, &ki))
+    {
+        printf("unknown key=%d\n", e->key());
+        return;
+    }
 
-    spkb_kbstate[key].state = 1;
-    spkb_kbstate[key].press = 1234;
-    spkb_kbstate[key].frame = sp_int_ctr;
+    spkb_last.keysym = ks;
+    spkb_last.shifted = shks;
+    spkb_last.modif = 0;
+    spkb_last.index = ki;
+
+    spkb_kbstate[ki].state = 1;
+    spkb_kbstate[ki].press = 1234;
+    spkb_kbstate[ki].frame = sp_int_ctr;
 
     spkb_state_changed = 1;
     process_keys();
@@ -87,35 +153,19 @@ void QSpectemu::keyPressEvent(QKeyEvent *e)
 
 void QSpectemu::keyReleaseEvent(QKeyEvent *e)
 {
-    int key = e->key();
-    spkb_kbstate[key].state = 0;
-    spkb_kbstate[key].press = 0;
+    int ks, shks, ki;
+    if(!decodeKey(e->key(), &ks, &shks, &ki))
+    {
+        printf("unknown key=%d\n", e->key());
+        return;
+    }
+
+    spkb_kbstate[ki].state = 0;
+    spkb_kbstate[ki].press = 0;
 
     spkb_state_changed = 1;
     process_keys();
 }
-
-//void QSpectemu::okClicked()
-//{
-//    if(spkb_kbstate[97].state == 0)
-//    {
-//        spkb_last.keysym = 97;
-//        spkb_last.shifted = 65;
-//        spkb_last.modif = 0;
-//        spkb_last.index = 97;
-//
-//        spkb_kbstate[97].state = 1;
-//        spkb_kbstate[97].press = 1234;
-//        spkb_kbstate[97].frame = sp_int_ctr;
-//    }
-//    else
-//    {
-//        spkb_kbstate[97].state = 0;
-//        spkb_kbstate[97].press = 0;
-//    }
-//    spkb_state_changed = 1;
-//    process_keys();
-//}
 
 #ifdef	__cplusplus
 extern	"C" {
@@ -176,7 +226,13 @@ int display_keyboard(void)
 
 void update_screen(void)
 {
-    qspectemu->update();
+    if(updating)
+    {
+        return;
+    }
+    updating = true;
+    QTimer::singleShot(100, qspectemu, SLOT(update()));
+    //qspectemu->update();
 }
 
 void destroy_spect_scr(void)
